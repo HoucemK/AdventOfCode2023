280ac96594fd042f63392c471842b301820b1d3c
Houcem Krichen <houcem.krichen@radiofrance.com>
2023-12-01 11:18:19 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long =
+            input
+                .map { it.filter { it in '0'..'9' } }
+                .map { "${it.first()}${it.last()}" }.sumOf { it.toLong() }
+
+
+    fun part2(input: List<String>): Long {
+        val numbers = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        val l = input.map { s ->
+            val result = buildString {
+                for (i in s.indices)
+                    if (s[i] in '0'..'9')
+                        append(s[i])
+                    else {
+                        val substring = s.substring(i)
+                        for (j in numbers.indices){
+                            if (substring.startsWith(numbers[j]))
+                                append(j+1)
+                        }
+                    }
+            }
+            "${result.first()}${result.last()}".toLong()
+        }
+       return l.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("day1")
     part1(input).println()
     part2(input).println()
 }
